785cc306aa61cae66d6c7ca0e29e54bd211f3934
Jacob Ransom <jacob@innocraft.com>
2025-08-11 11:13:23 +1200

    Added error handling

--- API.php
+++ API.php
@@ -9,6 +9,7 @@
 
 namespace Piwik\Plugins\OpenApiDocs;
 
+use Piwik\Common;
 use Piwik\Piwik;
 use Piwik\Plugins\OpenApiDocs\Generate\MatomoApiDocGenerator;
 
@@ -22,16 +23,27 @@ class API extends \Piwik\Plugin\API
     /**
      * Get the generated API documentation data for the specified plugin.
      *
-     * /index.php?module=API&method=OpenApiDocs.getApiDocumentationJson&plugin=CustomAlerts
+     * /index.php?module=API&method=OpenApiDocs.getGeneratedOpenApiSpec&plugin=CustomAlerts
      *
      * @param string $plugin Name of the plugin to get the JSON for. E.g. TagManager or CustomerAlerts
      * @param string $format Optional format string to indicate JSON or YAML. Default is JSON
      * @return string | array
      * @throws \Exception
      */
-    public function getApiDocumentation(string $plugin, string $format = 'json')
+    public function getGeneratedOpenApiSpec(string $plugin, string $format = '')
     {
         Piwik::checkUserHasSomeViewAccess();
+
+        // Return an error if format is something other than JSON or YAML
+        $allowedFormats = ['json', 'yaml'];
+        if (!in_array(strtolower($format), $allowedFormats)) {
+            throw new \Exception(
+                Piwik::translate(
+                    'General_ExceptionInvalidReportRendererFormat',
+                    [$format, implode(', ', $allowedFormats)]
+                )
+            );
+        }
 
         $docString = (new MatomoApiDocGenerator())->generatePluginDoc($plugin, $format);
         return strtolower($format) === 'json' ? json_decode($docString, true) : $docString;
