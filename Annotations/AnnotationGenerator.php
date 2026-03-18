matomo-org/plugin-ApiReference

commit dd13be5f591436d255fc695d1dbf720dd3771a4e

Added ability to add documentation for inactive plugins, PG-4593 (#23)
* inactive plugins now can have generated specs

* changelog

* Added guards for cloud plugins

* Added extra plugins and made const

Annotations/AnnotationGenerator.php
@@ -15,6 +15,7 @@ use Matomo\Dependencies\OpenApiDocs\phpDocumentor\Reflection\DocBlock\Descriptio
 use Matomo\Dependencies\OpenApiDocs\phpDocumentor\Reflection\DocBlock\Tags\Param;
 use Matomo\Dependencies\OpenApiDocs\phpDocumentor\Reflection\DocBlock\Tags\TagWithType;
 use Matomo\Dependencies\OpenApiDocs\phpDocumentor\Reflection\DocBlockFactory;
+use Piwik\Exception\PluginNotFoundException;
 use Piwik\API\DocumentationGenerator;
 use Piwik\API\NoDefaultValue;
 use Piwik\API\Proxy;
@@ -98,13 +99,20 @@ class AnnotationGenerator
      *
      * @return string[]|array[] The collection of all the lines which make up the generated annotations for the public API
      * endpoints defined by the plugin.
-     * @throws \Piwik\Exception\PluginDeactivatedException If the plugin is not activated. It should be loaded.
+     * @throws PluginNotFoundException If the plugin is not present in the filesystem.
      * @throws \Throwable
      */
     public function generatePluginApiAnnotations(string $pluginName, bool $writeToFile = false): array
     {
         BaseValidator::check('plugin', $pluginName, [new NotEmpty()]);
-        Manager::getInstance()->checkIsPluginActivated($pluginName);
+
+        if (in_array($pluginName, OpenApiDocs::PLUGIN_BLOCKLIST, true)) {
+            throw new \RuntimeException('OpenAPI doc generation is blocked for ' . $pluginName . '.');
+        }
+
+        if (!Manager::getInstance()->isPluginInFilesystem($pluginName)) {
+            throw new PluginNotFoundException($pluginName);
+        }
 
         $rules = require $this->currentPluginDir . '/Annotations/config.php';
         $pluginAnnotationDir = $this->currentPluginDir . OpenApiDocs::GENERATED_ANNOTATIONS_PATH;
@@ -1180,24 +1188,27 @@ class AnnotationGenerator
         $responseSchema = !empty($responseInfo['type']) ? $this->buildSchemaObjectArray($responseInfo['type']) : [];
 
         $mediaTypes = [];
-        // This simply reuses the example URLs used by the current documentation, but some endpoints don't work because authentication is required
-        $exampleUrls = $this->getApplicableDemoExampleUrls($plugin, $method, $paramsData);
-        foreach ($exampleUrls as $type => $url) {
-            $exampleValue = $this->getExampleIfAvailable($url);
-            // If the example lookup failed, try making the same request locally using a local token.
-            if (empty($exampleValue)) {
-                $exampleValue = $this->getExampleIfAvailable($url, true);
-            }
-            if (strlen($exampleValue) > self::EXAMPLE_CHAR_LIMIT) {
-                $exampleValue = $this->cutExampleCloseToCharLimit($exampleValue, $type);
-            }
+        $exampleUrls = [];
+        if (Manager::getInstance()->isPluginActivated($plugin)) {
+            // Only fetch live examples for activated plugins since their endpoints can be executed safely.
+            $exampleUrls = $this->getApplicableDemoExampleUrls($plugin, $method, $paramsData);
+            foreach ($exampleUrls as $type => $url) {
+                $exampleValue = $this->getExampleIfAvailable($url);
+                // If the example lookup failed, try making the same request locally using a local token.
+                if (empty($exampleValue)) {
+                    $exampleValue = $this->getExampleIfAvailable($url, true);
+                }
+                if (strlen($exampleValue) > self::EXAMPLE_CHAR_LIMIT) {
+                    $exampleValue = $this->cutExampleCloseToCharLimit($exampleValue, $type);
+                }
 
-            // Skip if there was no example response
-            if (empty($exampleValue)) {
-                continue;
-            }
+                // Skip if there was no example response
+                if (empty($exampleValue)) {
+                    continue;
+                }
 
-            $mediaTypes[] = $this->buildMediaTypePropertiesArray($type, $exampleValue, $responseSchema);
+                $mediaTypes[] = $this->buildMediaTypePropertiesArray($type, $exampleValue, $responseSchema);
+            }
         }
 
         // Check if any example files exist even though there aren't any example URLs

Annotations/ApiMethodInfoExtractor.php
@@ -11,6 +11,7 @@ declare(strict_types=1);
 
 namespace Piwik\Plugins\OpenApiDocs\Annotations;
 
+use Piwik\Exception\PluginNotFoundException;
 use Piwik\API\Proxy;
 use Piwik\API\Request;
 use Piwik\Plugin\Manager;
@@ -42,7 +43,9 @@ class ApiMethodInfoExtractor
         $methodInfoArray = [];
         foreach ($pluginNames as $plugin) {
             BaseValidator::check('pluginName', $plugin, [new NotEmpty()]);
-            Manager::getInstance()->checkIsPluginActivated($plugin);
+            if (!Manager::getInstance()->isPluginInFilesystem($plugin)) {
+                throw new PluginNotFoundException($plugin);
+            }
 
             $className = Request::getClassNameAPI($plugin);
 

OpenApiDocs.php
@@ -18,6 +18,7 @@ class OpenApiDocs extends \Piwik\Plugin
     public const EXAMPLE_RESPONSES_PATH = '/tmp/responses/';
     public const GENERATED_SPECS_PATH = '/tmp/specs/';
     public const AVAILABLE_PROPERTY_TYPES = ['string', 'number', 'integer', 'boolean', 'array', 'object', 'null'];
+    public const PLUGIN_BLOCKLIST = ['Billing', 'Cloud', 'ConnectAccounts', 'CDN', 'ProxySite'];
 
     public function registerEvents()
     {

Specs/SpecGenerator.php
@@ -12,6 +12,7 @@ namespace Piwik\Plugins\OpenApiDocs\Specs;
 use Matomo\Dependencies\OpenApiDocs\OpenApi\Annotations\OpenApi;
 use Matomo\Dependencies\OpenApiDocs\OpenApi\Generator;
 use Piwik\Container\StaticContainer;
+use Piwik\Exception\PluginNotFoundException;
 use Piwik\Log\LoggerInterface;
 use Piwik\Log\NullLogger;
 use Piwik\Plugin\Manager;
@@ -45,6 +46,12 @@ class SpecGenerator
     {
         BaseValidator::check('plugin', $pluginName, [new NotEmpty()]);
 
+        foreach (explode(',', $pluginName) as $currentPluginName) {
+            if (in_array($currentPluginName, OpenApiDocs::PLUGIN_BLOCKLIST, true)) {
+                throw new \RuntimeException('OpenAPI doc generation is blocked for ' . $currentPluginName . '.');
+            }
+        }
+
         return $this->generateSpec(explode(',', $pluginName), $format, $version, $writeToFile);
     }
 
@@ -59,7 +66,7 @@ class SpecGenerator
      * @return string
      * @throws \Piwik\Exception\DI\DependencyException
      * @throws \Piwik\Exception\DI\NotFoundException
-     * @throws \Piwik\Exception\PluginDeactivatedException
+     * @throws PluginNotFoundException
      * @throws \Exception
      */
     public function generateSpec(array $pluginNames, string $format = 'json', string $version = OpenApiDocs::DEFAULT_SPEC_VERSION, bool $writeToFile = false): string
@@ -70,7 +77,9 @@ class SpecGenerator
         $pluginDirs = [];
         foreach ($pluginNames as $pluginName) {
             BaseValidator::check('pluginName', $pluginName, [new NotEmpty()]);
-            Manager::getInstance()->checkIsPluginActivated($pluginName);
+            if (!Manager::getInstance()->isPluginInFilesystem($pluginName)) {
+                throw new PluginNotFoundException($pluginName);
+            }
 
             $pluginAnnotationsSource = $currentPluginDir . '/tmp/annotations/' . $pluginName . 'GeneratedAnnotations.php';
             try {

commit ae0260c777a64772a63ea671358fa7ac854338de

Fixed broken description logic from PR #23, added back in early return

Annotations/AnnotationGenerator.php
@@ -357,7 +357,7 @@ class AnnotationGenerator
             $responseInfo['description'] = 'Response of unknown type';
         }
         if (!empty($returnTag->getDescription())) {
-            $responseInfo['description'] = $returnTag->getDescription();
+            $responseInfo['description'] = $this->getDescriptionText($returnTag->getDescription());
         }
 
         return $responseInfo;
@@ -545,6 +545,22 @@ class AnnotationGenerator
     {
         $description = str_replace("\n", ' ', trim($description));
         return str_replace('"', '""', $description);
+    }
+
+    /**
+     * Normalise phpDocumentor description values into plain strings.
+     *
+     * @param mixed $description
+     *
+     * @return string
+     */
+    protected function getDescriptionText($description): string
+    {
+        if ($description instanceof Description) {
+            return $description->getBodyTemplate();
+        }
+
+        return is_string($description) ? $description : '';
     }
 
     /**
@@ -1149,10 +1165,7 @@ class AnnotationGenerator
             $successArray['ref'] = '#/components/responses/GenericSuccess';
         }
 
-        $description = $responseInfo['description'] ?? null;
-        if ($description instanceof Description) {
-            $description = $description->getBodyTemplate();
-        }
+        $description = $this->getDescriptionText($responseInfo['description'] ?? null);
 
         // If it's a generic type and there's no custom description, use one of the global generic responses
         if (empty($successArray['ref']) && !empty($responseInfo['type']) && empty($description)) {
@@ -1927,17 +1940,19 @@ class AnnotationGenerator
             $operationValuesMap[] = ['@OA\Parameter' => $paramMap];
         }
         foreach ($responses as $response) {
+            $responseDescription = $this->getDescriptionText($response['description'] ?? null);
+
             // Don't use the reference if there are media type examples
             if (isset($response['ref']) && empty($response['mediaTypes'])) {
                 $code = $response['code'];
                 $codeFormatted = is_numeric($code) ? (string)$code : '"' . $code . '"';
-                $description = !empty($response['description']) && strpos($response['description'], 'Example links: [') !== false
-                    ? ', description="' . $this->normaliseDescriptionText($response['description']) . '"' : '';
+                $description = $responseDescription !== '' && strpos($responseDescription, 'Example links: [') !== false
+                    ? ', description="' . $this->normaliseDescriptionText($responseDescription) . '"' : '';
                 $operationValuesMap[] = '@OA\Response(response=' . $codeFormatted . $description . ', ref="' . $response['ref'] . '")';
             } else {
                 $responsePropertyArray = [
                     'response=200',
-                    'description="' . $this->normaliseDescriptionText($response['description'] ?? 'OK') . '"',
+                    'description="' . $this->normaliseDescriptionText($responseDescription !== '' ? $responseDescription : 'OK') . '"',
                 ];
                 if (!empty($response['schema'])) {
                     $responsePropertyArray = array_merge($responsePropertyArray, $response['schema']);

Commands/GenerateSpecFile.php
@@ -127,6 +127,7 @@ class GenerateSpecFile extends ConsoleCommand
 
         if ($notDryRun) {
             $output->writeln('<info>Results written to plugins/OpenApiDocs/tmp/specs/ directory.</info>');
+            return self::SUCCESS;
         }
 
         $output->writeln($result);