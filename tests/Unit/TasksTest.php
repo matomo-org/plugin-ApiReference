matomo-org/plugin-ApiReference

commit dd13be5f591436d255fc695d1dbf720dd3771a4e

Added ability to add documentation for inactive plugins, PG-4593 (#23)
* inactive plugins now can have generated specs

* changelog

* Added guards for cloud plugins

* Added extra plugins and made const

Annotations/AnnotationGenerator.php
@@ -15,6 +15,7 @@ use Matomo\Dependencies\OpenApiDocs\phpDocumentor\Reflection\DocBlock\Descriptio
 use Matomo\Dependencies\OpenApiDocs\phpDocumentor\Reflection\DocBlock\Tags\Param;
 use Matomo\Dependencies\OpenApiDocs\phpDocumentor\Reflection\DocBlock\Tags\TagWithType;
 use Matomo\Dependencies\OpenApiDocs\phpDocumentor\Reflection\DocBlockFactory;
+use Piwik\Exception\PluginNotFoundException;
 use Piwik\API\DocumentationGenerator;
 use Piwik\API\NoDefaultValue;
 use Piwik\API\Proxy;
@@ -98,13 +99,20 @@ class AnnotationGenerator
      *
      * @return string[]|array[] The collection of all the lines which make up the generated annotations for the public API
      * endpoints defined by the plugin.
-     * @throws \Piwik\Exception\PluginDeactivatedException If the plugin is not activated. It should be loaded.
+     * @throws PluginNotFoundException If the plugin is not present in the filesystem.
      * @throws \Throwable
      */
     public function generatePluginApiAnnotations(string $pluginName, bool $writeToFile = false): array
     {
         BaseValidator::check('plugin', $pluginName, [new NotEmpty()]);
-        Manager::getInstance()->checkIsPluginActivated($pluginName);
+
+        if (in_array($pluginName, OpenApiDocs::PLUGIN_BLOCKLIST, true)) {
+            throw new \RuntimeException('OpenAPI doc generation is blocked for ' . $pluginName . '.');
+        }
+
+        if (!Manager::getInstance()->isPluginInFilesystem($pluginName)) {
+            throw new PluginNotFoundException($pluginName);
+        }
 
         $rules = require $this->currentPluginDir . '/Annotations/config.php';
         $pluginAnnotationDir = $this->currentPluginDir . OpenApiDocs::GENERATED_ANNOTATIONS_PATH;
@@ -1180,24 +1188,27 @@ class AnnotationGenerator
         $responseSchema = !empty($responseInfo['type']) ? $this->buildSchemaObjectArray($responseInfo['type']) : [];
 
         $mediaTypes = [];
-        // This simply reuses the example URLs used by the current documentation, but some endpoints don't work because authentication is required
-        $exampleUrls = $this->getApplicableDemoExampleUrls($plugin, $method, $paramsData);
-        foreach ($exampleUrls as $type => $url) {
-            $exampleValue = $this->getExampleIfAvailable($url);
-            // If the example lookup failed, try making the same request locally using a local token.
-            if (empty($exampleValue)) {
-                $exampleValue = $this->getExampleIfAvailable($url, true);
-            }
-            if (strlen($exampleValue) > self::EXAMPLE_CHAR_LIMIT) {
-                $exampleValue = $this->cutExampleCloseToCharLimit($exampleValue, $type);
-            }
+        $exampleUrls = [];
+        if (Manager::getInstance()->isPluginActivated($plugin)) {
+            // Only fetch live examples for activated plugins since their endpoints can be executed safely.
+            $exampleUrls = $this->getApplicableDemoExampleUrls($plugin, $method, $paramsData);
+            foreach ($exampleUrls as $type => $url) {
+                $exampleValue = $this->getExampleIfAvailable($url);
+                // If the example lookup failed, try making the same request locally using a local token.
+                if (empty($exampleValue)) {
+                    $exampleValue = $this->getExampleIfAvailable($url, true);
+                }
+                if (strlen($exampleValue) > self::EXAMPLE_CHAR_LIMIT) {
+                    $exampleValue = $this->cutExampleCloseToCharLimit($exampleValue, $type);
+                }
 
-            // Skip if there was no example response
-            if (empty($exampleValue)) {
-                continue;
-            }
+                // Skip if there was no example response
+                if (empty($exampleValue)) {
+                    continue;
+                }
 
-            $mediaTypes[] = $this->buildMediaTypePropertiesArray($type, $exampleValue, $responseSchema);
+                $mediaTypes[] = $this->buildMediaTypePropertiesArray($type, $exampleValue, $responseSchema);
+            }
         }
 
         // Check if any example files exist even though there aren't any example URLs

Specs/SpecGenerator.php
@@ -12,6 +12,7 @@ namespace Piwik\Plugins\OpenApiDocs\Specs;
 use Matomo\Dependencies\OpenApiDocs\OpenApi\Annotations\OpenApi;
 use Matomo\Dependencies\OpenApiDocs\OpenApi\Generator;
 use Piwik\Container\StaticContainer;
+use Piwik\Exception\PluginNotFoundException;
 use Piwik\Log\LoggerInterface;
 use Piwik\Log\NullLogger;
 use Piwik\Plugin\Manager;
@@ -45,6 +46,12 @@ class SpecGenerator
     {
         BaseValidator::check('plugin', $pluginName, [new NotEmpty()]);
 
+        foreach (explode(',', $pluginName) as $currentPluginName) {
+            if (in_array($currentPluginName, OpenApiDocs::PLUGIN_BLOCKLIST, true)) {
+                throw new \RuntimeException('OpenAPI doc generation is blocked for ' . $currentPluginName . '.');
+            }
+        }
+
         return $this->generateSpec(explode(',', $pluginName), $format, $version, $writeToFile);
     }
 
@@ -59,7 +66,7 @@ class SpecGenerator
      * @return string
      * @throws \Piwik\Exception\DI\DependencyException
      * @throws \Piwik\Exception\DI\NotFoundException
-     * @throws \Piwik\Exception\PluginDeactivatedException
+     * @throws PluginNotFoundException
      * @throws \Exception
      */
     public function generateSpec(array $pluginNames, string $format = 'json', string $version = OpenApiDocs::DEFAULT_SPEC_VERSION, bool $writeToFile = false): string
@@ -70,7 +77,9 @@ class SpecGenerator
         $pluginDirs = [];
         foreach ($pluginNames as $pluginName) {
             BaseValidator::check('pluginName', $pluginName, [new NotEmpty()]);
-            Manager::getInstance()->checkIsPluginActivated($pluginName);
+            if (!Manager::getInstance()->isPluginInFilesystem($pluginName)) {
+                throw new PluginNotFoundException($pluginName);
+            }
 
             $pluginAnnotationsSource = $currentPluginDir . '/tmp/annotations/' . $pluginName . 'GeneratedAnnotations.php';
             try {

Annotations/ApiMethodInfoExtractor.php
@@ -11,6 +11,7 @@ declare(strict_types=1);
 
 namespace Piwik\Plugins\OpenApiDocs\Annotations;
 
+use Piwik\Exception\PluginNotFoundException;
 use Piwik\API\Proxy;
 use Piwik\API\Request;
 use Piwik\Plugin\Manager;
@@ -42,7 +43,9 @@ class ApiMethodInfoExtractor
         $methodInfoArray = [];
         foreach ($pluginNames as $plugin) {
             BaseValidator::check('pluginName', $plugin, [new NotEmpty()]);
-            Manager::getInstance()->checkIsPluginActivated($plugin);
+            if (!Manager::getInstance()->isPluginInFilesystem($plugin)) {
+                throw new PluginNotFoundException($plugin);
+            }
 
             $className = Request::getClassNameAPI($plugin);
 

OpenApiDocs.php
@@ -18,6 +18,7 @@ class OpenApiDocs extends \Piwik\Plugin
     public const EXAMPLE_RESPONSES_PATH = '/tmp/responses/';
     public const GENERATED_SPECS_PATH = '/tmp/specs/';
     public const AVAILABLE_PROPERTY_TYPES = ['string', 'number', 'integer', 'boolean', 'array', 'object', 'null'];
+    public const PLUGIN_BLOCKLIST = ['Billing', 'Cloud', 'ConnectAccounts', 'CDN', 'ProxySite'];
 
     public function registerEvents()
     {

commit 6ae5e761e77f7d854dffeaae549188aeb9191504

Spec generation task, #PG-4593 (#26)
* Pulled out spec generation logic into common service, added new Task for spec generation

* Fixed plugin name logic and tests

* Fixed broken description logic from PR #23, added back in early return

* Add docs to public method

* Changed to daily and removed 'all' option

* fix tests

* Move blocklist validation down to generateSpec level

* removed config file, not needed

* restored config.php to normal state

* fix phpcs

Annotations/AnnotationGenerator.php
@@ -357,7 +357,7 @@ class AnnotationGenerator
             $responseInfo['description'] = 'Response of unknown type';
         }
         if (!empty($returnTag->getDescription())) {
-            $responseInfo['description'] = $returnTag->getDescription();
+            $responseInfo['description'] = $this->getDescriptionText($returnTag->getDescription());
         }
 
         return $responseInfo;
@@ -545,6 +545,22 @@ class AnnotationGenerator
     {
         $description = str_replace("\n", ' ', trim($description));
         return str_replace('"', '""', $description);
+    }
+
+    /**
+     * Normalise phpDocumentor description values into plain strings.
+     *
+     * @param mixed $description
+     *
+     * @return string
+     */
+    protected function getDescriptionText($description): string
+    {
+        if ($description instanceof Description) {
+            return $description->getBodyTemplate();
+        }
+
+        return is_string($description) ? $description : '';
     }
 
     /**
@@ -1149,10 +1165,7 @@ class AnnotationGenerator
             $successArray['ref'] = '#/components/responses/GenericSuccess';
         }
 
-        $description = $responseInfo['description'] ?? null;
-        if ($description instanceof Description) {
-            $description = $description->getBodyTemplate();
-        }
+        $description = $this->getDescriptionText($responseInfo['description'] ?? null);
 
         // If it's a generic type and there's no custom description, use one of the global generic responses
         if (empty($successArray['ref']) && !empty($responseInfo['type']) && empty($description)) {
@@ -1927,17 +1940,19 @@ class AnnotationGenerator
             $operationValuesMap[] = ['@OA\Parameter' => $paramMap];
         }
         foreach ($responses as $response) {
+            $responseDescription = $this->getDescriptionText($response['description'] ?? null);
+
             // Don't use the reference if there are media type examples
             if (isset($response['ref']) && empty($response['mediaTypes'])) {
                 $code = $response['code'];
                 $codeFormatted = is_numeric($code) ? (string)$code : '"' . $code . '"';
-                $description = !empty($response['description']) && strpos($response['description'], 'Example links: [') !== false
-                    ? ', description="' . $this->normaliseDescriptionText($response['description']) . '"' : '';
+                $description = $responseDescription !== '' && strpos($responseDescription, 'Example links: [') !== false
+                    ? ', description="' . $this->normaliseDescriptionText($responseDescription) . '"' : '';
                 $operationValuesMap[] = '@OA\Response(response=' . $codeFormatted . $description . ', ref="' . $response['ref'] . '")';
             } else {
                 $responsePropertyArray = [
                     'response=200',
-                    'description="' . $this->normaliseDescriptionText($response['description'] ?? 'OK') . '"',
+                    'description="' . $this->normaliseDescriptionText($responseDescription !== '' ? $responseDescription : 'OK') . '"',
                 ];
                 if (!empty($response['schema'])) {
                     $responsePropertyArray = array_merge($responsePropertyArray, $response['schema']);

Commands/GenerateSpecFile.php
@@ -11,8 +11,8 @@ namespace Piwik\Plugins\OpenApiDocs\Commands;
 
 use Piwik\Container\StaticContainer;
 use Piwik\Plugin\ConsoleCommand;
-use Piwik\Plugins\OpenApiDocs\Annotations\AnnotationGenerator;
-use Piwik\Plugins\OpenApiDocs\Specs\SpecGenerator;
+use Piwik\Plugins\OpenApiDocs\Generation\SpecGenerationService;
+use Piwik\Plugins\OpenApiDocs\OpenApiDocs;
 
 /**
  * This class lets you define a new command. To read more about commands have a look at our Matomo Console guide on
@@ -24,6 +24,18 @@ use Piwik\Plugins\OpenApiDocs\Specs\SpecGenerator;
 class GenerateSpecFile extends ConsoleCommand
 {
     /**
+     * @var SpecGenerationService
+     */
+    private $specGenerationService;
+
+    public function __construct(?SpecGenerationService $specGenerationService = null)
+    {
+        $this->specGenerationService = $specGenerationService ?: StaticContainer::get(SpecGenerationService::class);
+
+        parent::__construct();
+    }
+
+    /**
      * This method allows you to configure your command. Here you can define the name and description of your command
      * as well as all options and arguments you expect when executing it.
      */
@@ -31,7 +43,7 @@ class GenerateSpecFile extends ConsoleCommand
     {
         $this->setName('openapidocs:generate-spec-file');
         $this->setDescription('Generate the OpenAPI documentation file for the Matomo APIs.');
-        $this->addRequiredValueOption('plugin', 'p', 'Name of the plugin to document, use all to process every plugin');
+        $this->addRequiredValueOption('plugin', 'p', 'Name of the plugin to document. Multiple plugins can be comma-separated');
         $this->addRequiredValueOption('format', 'f', 'Format of the spec file (JSON or YAML). Default is JSON');
         $this->addRequiredValueOption('api-version', null, 'Version of the spec file. Default is 1.0.0');
         $this->addNoValueOption('not-dry-run', null, 'Flag to allow writing to file instead of outputting a dry run.');
@@ -78,17 +90,15 @@ class GenerateSpecFile extends ConsoleCommand
 
         $plugin = $input->getOption('plugin');
 
-
         if (empty($plugin)) {
             throw new \RuntimeException('Please specify a plugin name.');
         }
 
-        if (strtolower($plugin) == 'all') {
-            $plugins = require __DIR__ . '/../config/plugins.php';
-            $plugin = implode(',', $plugins);
-        }
+        $pluginNames = array_values(array_filter(array_map('trim', explode(',', $plugin)), static function (string $pluginName): bool {
+            return $pluginName !== '';
+        }));
         $format = $input->getOption('format') ?: 'json';
-        $version = $input->getOption('version') ?: '1.0.0';
+        $version = $input->getOption('api-version') ?: OpenApiDocs::DEFAULT_SPEC_VERSION;
         $notDryRun = $input->getOption('not-dry-run') ?: false;
         $addAnnotations = $input->getOption('add-annotations') ?: false;
 
@@ -96,24 +106,27 @@ class GenerateSpecFile extends ConsoleCommand
 
         $output->writeln($message);
 
+        $result = $this->specGenerationService->generateSpecForPlugins(
+            $plugin,
+            $format,
+            $version,
+            $notDryRun,
+            $addAnnotations
+        );
+
         if ($addAnnotations) {
-            $pluginsArray = explode(',', $plugin);
-            foreach ($pluginsArray as $pluginName) {
-                (StaticContainer::get(AnnotationGenerator::class))->generatePluginApiAnnotations($pluginName, true);
+            foreach ($pluginNames as $pluginName) {
                 $output->writeln('<info>Created Annotations for ' . $pluginName . ' and wrote results to plugins/OpenApiDocs/tmp/annotations.</info>');
             }
         }
 
-        $result = (new SpecGenerator())->generatePluginDoc($plugin, $format, $version, $notDryRun);
-
         if ($notDryRun) {
             $output->writeln('<info>Results written to plugins/OpenApiDocs/tmp/specs/ directory.</info>');
-
-            return $result ? self::SUCCESS : self::FAILURE;
+            return self::SUCCESS;
         }
 
         $output->writeln($result);
 
-        return $result ? self::SUCCESS : self::FAILURE;
+        return self::SUCCESS;
     }
 }

Generation/SpecGenerationService.php
@@ -1,0 +1,88 @@
+<?php
+
+/**
+ * Matomo - free/libre analytics platform
+ *
+ * @link    https://matomo.org
+ * @license https://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
+ */
+
+declare(strict_types=1);
+
+namespace Piwik\Plugins\OpenApiDocs\Generation;
+
+use Piwik\Plugins\OpenApiDocs\Annotations\AnnotationGenerator;
+use Piwik\Plugins\OpenApiDocs\OpenApiDocs;
+use Piwik\Plugins\OpenApiDocs\Specs\SpecGenerator;
+
+class SpecGenerationService
+{
+    /**
+     * @var AnnotationGenerator
+     */
+    private $annotationGenerator;
+
+    /**
+     * @var SpecGenerator
+     */
+    private $specGenerator;
+
+    public function __construct(AnnotationGenerator $annotationGenerator, SpecGenerator $specGenerator)
+    {
+        $this->annotationGenerator = $annotationGenerator;
+        $this->specGenerator = $specGenerator;
+    }
+
+    /**
+     * Generate an OpenAPI spec for one or more comma-separated plugin names.
+     *
+     * @param string $pluginNames Comma-separated plugin names to include in the generated spec.
+     * @param string $format Output format for the spec, for example `json` or `yaml`.
+     * @param string $version Version string written into the generated OpenAPI spec.
+     * @param bool $writeToFile Whether the generated spec should also be written to the plugin tmp specs directory.
+     * @param bool $addAnnotations Whether API annotations should be regenerated before building the spec.
+     * @return string The generated OpenAPI spec contents.
+     * @throws \RuntimeException If no non-empty plugin names are provided.
+     */
+    public function generateSpecForPlugins(
+        string $pluginNames,
+        string $format = 'json',
+        string $version = OpenApiDocs::DEFAULT_SPEC_VERSION,
+        bool $writeToFile = false,
+        bool $addAnnotations = false
+    ): string {
+        $parsedPluginNames = $this->getPluginNames($pluginNames);
+
+        if ($addAnnotations) {
+            $this->generateAnnotations($parsedPluginNames);
+        }
+
+        return $this->specGenerator->generateSpec($parsedPluginNames, $format, $version, $writeToFile);
+    }
+
+    /**
+     * @param string[] $pluginNames
+     */
+    private function generateAnnotations(array $pluginNames): void
+    {
+        foreach ($pluginNames as $pluginName) {
+            $this->annotationGenerator->generatePluginApiAnnotations($pluginName, true);
+        }
+    }
+
+    /**
+     * @return string[]
+     */
+    private function getPluginNames(string $pluginNames): array
+    {
+        $plugins = array_filter(array_map('trim', explode(',', $pluginNames)), static function (string $pluginName): bool {
+            return $pluginName !== '';
+        });
+
+        if (empty($plugins)) {
+            throw new \RuntimeException('At least one plugin name is required.');
+        }
+
+        return array_values($plugins);
+    }
+}

Tasks.php
@@ -1,0 +1,72 @@
+<?php
+
+/**
+ * Matomo - free/libre analytics platform
+ *
+ * @link    https://matomo.org
+ * @license https://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
+ */
+
+declare(strict_types=1);
+
+namespace Piwik\Plugins\OpenApiDocs;
+
+use Piwik\Config;
+use Piwik\Log\LoggerInterface;
+use Piwik\Plugins\OpenApiDocs\Generation\SpecGenerationService;
+
+class Tasks extends \Piwik\Plugin\Tasks
+{
+    /**
+     * @var SpecGenerationService
+     */
+    private $specGenerationService;
+
+    /**
+     * @var LoggerInterface
+     */
+    private $logger;
+
+    public function __construct(SpecGenerationService $specGenerationService, LoggerInterface $logger)
+    {
+        $this->specGenerationService = $specGenerationService;
+        $this->logger = $logger;
+    }
+
+    public function schedule()
+    {
+        if ($this->isSpecGenerationEnabled()) {
+            $this->daily('generateConfiguredPluginSpecs');
+        }
+    }
+
+    public function generateConfiguredPluginSpecs(): void
+    {
+        $pluginNames = require __DIR__ . '/config/plugins.php';
+
+        foreach ($pluginNames as $pluginName) {
+            try {
+                $this->specGenerationService->generateSpecForPlugins(
+                    $pluginName,
+                    'json',
+                    OpenApiDocs::DEFAULT_SPEC_VERSION,
+                    true,
+                    true
+                );
+            } catch (\Throwable $e) {
+                $this->logger->error(
+                    'OpenApiDocs scheduled generation failed for plugin {plugin}: {error}',
+                    [
+                        'plugin' => $pluginName,
+                        'error' => $e->getMessage(),
+                    ]
+                );
+            }
+        }
+    }
+
+    private function isSpecGenerationEnabled(): bool
+    {
+        return (bool) (Config::getInstance()->OpenApiDocs['enable_spec_generation_task'] ?? 0);
+    }
+}

Specs/SpecGenerator.php
@@ -44,14 +44,6 @@ class SpecGenerator
      */
     public function generatePluginDoc(string $pluginName, string $format = 'json', string $version = OpenApiDocs::DEFAULT_SPEC_VERSION, bool $writeToFile = false): string
     {
-        BaseValidator::check('plugin', $pluginName, [new NotEmpty()]);
-
-        foreach (explode(',', $pluginName) as $currentPluginName) {
-            if (in_array($currentPluginName, OpenApiDocs::PLUGIN_BLOCKLIST, true)) {
-                throw new \RuntimeException('OpenAPI doc generation is blocked for ' . $currentPluginName . '.');
-            }
-        }
-
         return $this->generateSpec(explode(',', $pluginName), $format, $version, $writeToFile);
     }
 
@@ -73,6 +65,12 @@ class SpecGenerator
     {
         BaseValidator::check('pluginNames', $pluginNames, [new NotEmpty()]);
         $currentPluginDir = Manager::getInstance()::getPluginDirectory('OpenApiDocs');
+
+        foreach ($pluginNames as $currentPluginName) {
+            if (in_array($currentPluginName, OpenApiDocs::PLUGIN_BLOCKLIST, true)) {
+                throw new \RuntimeException('OpenAPI doc generation is blocked for ' . $currentPluginName . '.');
+            }
+        }
 
         $pluginDirs = [];
         foreach ($pluginNames as $pluginName) {

tests/Unit/Generation/SpecGenerationServiceTest.php
@@ -1,0 +1,61 @@
+<?php
+
+/**
+ * Matomo - free/libre analytics platform
+ *
+ * @link    https://matomo.org
+ * @license https://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
+ */
+
+declare(strict_types=1);
+
+namespace Piwik\Plugins\OpenApiDocs\tests\Unit\Generation;
+
+require_once PIWIK_INCLUDE_PATH . '/plugins/OpenApiDocs/vendor/autoload.php';
+
+use PHPUnit\Framework\TestCase;
+use Piwik\Plugins\OpenApiDocs\Annotations\AnnotationGenerator;
+use Piwik\Plugins\OpenApiDocs\Generation\SpecGenerationService;
+use Piwik\Plugins\OpenApiDocs\Specs\SpecGenerator;
+
+/**
+ * @group OpenApiDocs
+ * @group OpenApiDocs_Unit
+ * @group OpenApiDocs_SpecGenerationServiceTest
+ */
+class SpecGenerationServiceTest extends TestCase
+{
+    public function testGenerateSpecForPluginsThrowsForEmptyPluginNames(): void
+    {
+        $service = new SpecGenerationService(
+            $this->createStub(AnnotationGenerator::class),
+            $this->createStub(SpecGenerator::class)
+        );
+
+        $this->expectException(\RuntimeException::class);
+        $this->expectExceptionMessage('At least one plugin name is required.');
+
+        $service->generateSpecForPlugins(' , ', 'json', '1.0.0', false, false);
+    }
+
+    public function testGenerateSpecForPluginsWorksForValidPluginName(): void
+    {
+        $annotationGenerator = $this->createMock(AnnotationGenerator::class);
+        $specGenerator = $this->getMockBuilder(SpecGenerator::class)
+            ->disableOriginalConstructor()
+            ->onlyMethods(['generateSpec'])
+            ->getMock();
+
+        $annotationGenerator->expects($this->once())
+            ->method('generatePluginApiAnnotations')
+            ->with('CustomAlerts', true);
+        $specGenerator->expects($this->once())
+            ->method('generateSpec')
+            ->with(['CustomAlerts'], 'json', '1.0.0', true)
+            ->willReturn('spec body');
+
+        $service = new SpecGenerationService($annotationGenerator, $specGenerator);
+
+        $service->generateSpecForPlugins('CustomAlerts', 'json', '1.0.0', true, true);
+    }
+}

tests/Unit/TasksTest.php
@@ -1,0 +1,98 @@
+<?php
+
+/**
+ * Matomo - free/libre analytics platform
+ *
+ * @link    https://matomo.org
+ * @license https://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
+ */
+
+declare(strict_types=1);
+
+namespace Piwik\Plugins\OpenApiDocs\tests\Unit;
+
+require_once PIWIK_INCLUDE_PATH . '/plugins/OpenApiDocs/vendor/autoload.php';
+
+use PHPUnit\Framework\TestCase;
+use Piwik\Config;
+use Piwik\Plugins\OpenApiDocs\Generation\SpecGenerationService;
+use Piwik\Plugins\OpenApiDocs\Tasks;
+use Piwik\Scheduler\Schedule\Daily;
+use Piwik\Tests\Framework\Mock\FakeLogger;
+
+/**
+ * @group OpenApiDocs
+ * @group OpenApiDocs_Unit
+ * @group OpenApiDocs_TasksTest
+ */
+class TasksTest extends TestCase
+{
+    /**
+     * @var mixed
+     */
+    private $originalConfig;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->originalConfig = Config::getInstance()->OpenApiDocs ?? null;
+    }
+
+    protected function tearDown(): void
+    {
+        Config::getInstance()->OpenApiDocs = $this->originalConfig;
+
+        parent::tearDown();
+    }
+
+    public function testScheduleDoesNotRegisterTaskWhenDisabled(): void
+    {
+        Config::getInstance()->OpenApiDocs = ['enable_spec_generation_task' => 0];
+
+        $tasks = new Tasks($this->createMock(SpecGenerationService::class), new FakeLogger());
+        $tasks->schedule();
+
+        $this->assertCount(0, $tasks->getScheduledTasks());
+    }
+
+    public function testScheduleRegistersDailyTaskWhenEnabled(): void
+    {
+        Config::getInstance()->OpenApiDocs = ['enable_spec_generation_task' => 1];
+
+        $tasks = new Tasks($this->createMock(SpecGenerationService::class), new FakeLogger());
+        $tasks->schedule();
+
+        $scheduledTasks = $tasks->getScheduledTasks();
+
+        $this->assertCount(1, $scheduledTasks);
+        $this->assertSame('generateConfiguredPluginSpecs', $scheduledTasks[0]->getMethodName());
+        $this->assertInstanceOf(Daily::class, $scheduledTasks[0]->getScheduledTime());
+    }
+
+    public function testGenerateConfiguredPluginSpecsLogsPerPluginFailuresAndContinues(): void
+    {
+        $calledPlugins = [];
+        $service = $this->createMock(SpecGenerationService::class);
+        $service->expects($this->atLeast(2))
+            ->method('generateSpecForPlugins')
+            ->willReturnCallback(function (string $pluginName) use (&$calledPlugins): string {
+                $calledPlugins[] = $pluginName;
+
+                if ($pluginName === 'RollUpReporting') {
+                    throw new \RuntimeException('Foo failed');
+                }
+
+                return 'ok';
+            });
+
+        $logger = new FakeLogger();
+        $tasks = new Tasks($service, $logger);
+
+        $tasks->generateConfiguredPluginSpecs();
+
+        $this->assertSame('RollUpReporting', $calledPlugins[0]);
+        $this->assertSame('Login', $calledPlugins[1]);
+        $this->assertStringContainsString('OpenApiDocs scheduled generation failed for plugin RollUpReporting: Foo failed', $logger->output);
+    }
+}