2cdcc793d3d9505d852c86fa900f1bd1c47f035a
Lachlan Reynolds <lachlan.reynolds@innocraft.com>
2026-05-21 18:26:53 +1200

    add hash param for learn more

--- vue/src/SwaggerPage/SwaggerUiPanel.vue
+++ vue/src/SwaggerPage/SwaggerUiPanel.vue
@@ -135,7 +135,7 @@ export default defineComponent({
       // TODO: replace with the final FAQ page once it is published.
       return translate(
         'ApiReference_SwaggerPageSpecNotAvailable',
-        externalLink('https://matomo.org/?post_type=faq&p=96250&preview=true'),
+        externalLink('https://matomo.org/?post_type=faq&p=96250&preview=true#why-is-the-openapi-specification-file-not-generated'),
         '</a>',
       );
     },
